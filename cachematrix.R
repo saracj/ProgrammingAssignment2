610bf156f13826a0a2303e9eacaf9e31ffd47fa4
saracj <sara.jonvik@gmail.com>
2018-01-22 12:54:32 +0100

    Added a draft of the two functions based on the examples provided in the assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+###################################################################################
+####### Simples functions that cache the inverse of a matrix in order to  #########
+####### avoid repeat calculations of very large matrices                  #########
+###################################################################################
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(A = matrix()) {
+    ## Functions used to chache the inverse of a matrix, in order
+    ## to avoid repeat calculations for large matrices (even though you really
+    ## cannot assume that all matrices can be solved with the solved routine, that 
+    ## is just silly). Creates a special matrix object where the inverse can be 
+    ## cached/stored after the solve routine has been used once
+    
+    mat <- NULL
+    set <- function(y){
+        A <<- y
+        mat <<- NULL
+    }
+    get <- function() A
+    setinv <- function(solve) mat <<- solve
+    getinv <- function() mat
+    list(set = set, get = get, 
+         setinv = setinv, 
+         getinv = getinv) 
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(A, ...) {
+    ## Gives the inverse of the special matrix A in the function makeCacheMatrix using the solve() routine 
+    ## in R. If the inverse has already been calculated, this function simply retrieves the calculated 
+    ## inverse of the matrix
+    
+    
+    mat <- A$getinv()
+    if(!is.null(mat)){
+        message("Getting chached matrix")
+        return(mat)
+    }
+    data <- A$get
+    mat <- solve(data, ...)
+    A$setinv(mat)
+    mat
 }
+
+
+
